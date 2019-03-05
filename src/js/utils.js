35c4dae11fc6be42fffc4f958772ae47d8721965
evb0110 <7149553@gmail.com>
2019-03-06 00:37:30 +0300

    made #4 playfield without tests

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,14 @@
 export function calcTileType(index, boardSize) {
   // TODO: write logic here
+  if (index === 0) return 'top-left';
+  if (index === boardSize - 1) return 'top-right';
+  if (index === boardSize * (boardSize - 1)) return 'bottom-left';
+  if (index === boardSize ** 2 - 1) return 'bottom-right'
+  if (index < boardSize - 1) return 'top';
+  if (index % boardSize === 0) return 'left';
+  if (index / boardSize >= boardSize - 1) return 'bottom';
+  if (index % boardSize === boardSize - 1) return 'right'
+
   return 'center';
 }
 
