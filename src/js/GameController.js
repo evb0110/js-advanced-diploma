198f8438758ad68767f6d8bc7f5245ef00a25315
evb0110 <7149553@gmail.com>
2019-03-14 17:12:15 +0300

    method makeTeam() in controller, made it more general

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,11 +1,16 @@
 import themes from './themes';
-import generateTeam, { generatePosition, generateTeamPositions } from './generators';
+import generateTeam, {
+  generatePosition,
+  generateTeamPositions
+} from './generators';
 import tooltips from './helpers/tooltips';
-import { moveIndices, attackIndices } from './helpers/reachIndices';
+import {
+  moveIndices,
+  attackIndices
+} from './helpers/reachIndices';
 import GamePlay from './GamePlay';
 import GameState from './GameState';
 import cursors from './cursors';
-import { pairToNumber, numberToPair } from './helpers/pairsAndNumbers';
 
 import Bowman from './characters/Bowman';
 import Daemon from './characters/Daemon';
@@ -29,29 +34,44 @@ export default class GameController {
     this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
     this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
 
+    this.makeTeams();
+
+    this.gamePlay.redrawPositions(this.positions);
+  }
+
+  makeTeams() {
+    const { boardSize } = this.gamePlay;
+
     const humanAllowedTypes = [Bowman, Magician, Swordsman];
     const computerAllowedTypes = [Daemon, Undead, Vampire];
+
     const humanTeam = generateTeam(humanAllowedTypes, 1, 2);
     const computerTeam = generateTeam(computerAllowedTypes, 1, 2);
-    const humanTeamPositions = generateTeamPositions([0, 7], [0, 1], 2);
-    const computerTeamPositions = generateTeamPositions([0, 7], [6, 7], 2);
-    this.positions = [];
 
+    const humanTeamPositions = generateTeamPositions([0, boardSize - 1], [0, 1], 2);
+    const computerTeamPositions = generateTeamPositions([0, boardSize - 1], [boardSize - 2, boardSize - 1], 2);
 
-    this.positions[0] = new PositionedCharacter(humanTeam[0], humanTeamPositions[0]);
-    this.positions[1] = new PositionedCharacter(humanTeam[1], humanTeamPositions[1]);
+    this.humanPositionedTeam = [];
+    this.computerPositionedTeam = [];
 
-    this.positions[2] = new PositionedCharacter(computerTeam[0], computerTeamPositions[0]);
-    this.positions[3] = new PositionedCharacter(computerTeam[1], computerTeamPositions[1]);
-    this.gamePlay.redrawPositions(this.positions);
+    humanTeam.forEach((humanCharacter, i) => {
+      const positionedCharacter = new PositionedCharacter(humanCharacter, humanTeamPositions[i]);
+      this.humanPositionedTeam.push(positionedCharacter);
+    });
+    computerTeam.forEach((computerCharacter, i) => {
+      const positionedCharacter = new PositionedCharacter(computerCharacter, computerTeamPositions[i]);
+      this.computerPositionedTeam.push(positionedCharacter);
+    });
+
+    this.positions = [...this.humanPositionedTeam, ...this.computerPositionedTeam];
   }
 
   onCellClick(index) {
     // TODO: react to click
     const posCharacter = this.positions.filter(pos => pos.position === index);
     if (
-      posCharacter.length
-      && ['bowman', 'swordsman', 'magician'].includes(posCharacter[0].character.type)
+      posCharacter.length &&
+      ['bowman', 'swordsman', 'magician'].includes(posCharacter[0].character.type)
     ) {
       if (this.selected) {
         this.gamePlay.deselectCell(this.selected);
@@ -68,7 +88,10 @@ export default class GameController {
     const posCharacter = this.positions.filter(pos => pos.position === index);
     if (posCharacter.length) {
       const {
-        level, attack, defence, health,
+        level,
+        attack,
+        defence,
+        health,
       } = posCharacter[0].character;
       const message = tooltips({
         level,
